61e8bb2403a4c86265aef31b7f74ba4c60319bf9
Ross Williams <ross@ross-williams.net>
2022-10-21 10:14:15 -0400

    Add Ctrl-C/Enter stop/restart support for run.rb
    Modularized generating the command line, because it was
    getting too long to be on one line.

--- run.rb
+++ run.rb
@@ -1,19 +1,30 @@
+require 'shellwords'
+
 puts "cargo build --quiet"
 log_level = ENV.fetch('RUST_LOG', "info")
 number_of_peers = Integer(ENV.fetch("RAFT_TEST_PEERS", "3"))
+listen_addr = "127.0.0.1"
+command_name = "raft_for_beginners_too"
 
 peers = (0..number_of_peers-1).map {|i| 8000 + i}
 
 puts "tmux new-session -s raft_for_beginners_too -d"
 puts "tmux set -g remain-on-exit on"
+puts "tmux set -g history-limit 100000"
 
 (0..(peers.size - 1)).each do
-    this_node = peers[0]
+    this_node = "#{listen_addr}:#{peers[0]}".shellescape
     peers_for_this_node = peers[1..-1]
-    puts "tmux new-window -a -t raft_for_beginners_too:\\$ -n 127.0.0.1:#{this_node} -e 'RUST_LOG=#{log_level}' target/debug/raft_for_beginners_too -l 127.0.0.1:#{this_node} " + peers_for_this_node.map {|peer_port| "--peer 127.0.0.1:#{peer_port}" }.join(" ")
+    peer_args = peers_for_this_node.map {|peer_port| "--peer #{listen_addr.shellescape}:#{peer_port.to_s.shellescape}" }.join(" ")
+    command = "target/debug/raft_for_beginners_too -l #{this_node} " + peer_args
+    shell_loop = "sh -c 'while :; do trap \"kill -INT %1\" INT; #{command}; printf \"\\nProcess exited.\\nPress any key to restart...\\n\"; trap - INT; read; done'"
+    log_env = "RUST_LOG=#{log_level}".shellescape
+    tmux_session = "raft_for_beginners_too:$".shellescape
+
+        puts "tmux new-window -a -t #{tmux_session} -n #{this_node} -e #{log_env} #{shell_loop}"
     peers.rotate!
 end
 
-puts "trap 'pkill raft_for_beginners_too; trap - INT' INT"
+#puts "trap 'pkill raft_for_beginners_too; trap - INT' INT"
 #puts "tmux attach -t raft_for_beginners_too"
 #puts "wait"
